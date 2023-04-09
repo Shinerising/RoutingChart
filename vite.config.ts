9e1cc07b00929d5f94a159fe5be71d7876d33146
Apollo Wayne <waynezhaoyang@gmail.com>
2023-04-10 07:54:00 +0800

    Add Legacy Plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import eslint from "vite-plugin-eslint";
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslint()],
+  plugins: [
+    vue(),
+    eslint(),
+    legacy({
+      targets: ["defaults", "Chrome 49"],
+    }),
+  ],
   resolve: {
     alias: {
       "@": "/src",
