086ceeb874d3b81d8b8dfabe2f766f18f67cfcfe
Apollo Wayne <waynezhaoyang@gmail.com>
2023-04-24 16:11:02 +0800

    Add Functions

--- src/services/http.ts
+++ src/services/http.ts
@@ -44,6 +44,8 @@ switch (request.Url.LocalPath)
         return;
 }
 */
+
+import { format } from "date-fns";
 // Use XHR to request data from the server
 const requestData = (path: string, body: [string, string][]):Promise<string> => {
   return new Promise((resolve, reject) => {
@@ -62,8 +64,30 @@ const requestData = (path: string, body: [string, string][]):Promise<string> => 
     xhr.send(body.map((v) => v.join("=")).join("&"));
   });
 };
-
+const getFilename = (date:Date, shift: "day" | "night") => {
+    return `${format(date, "yyyyMMdd")}${shift === "day" ? "白班" : "夜班"}.mdb`;
+}
 const getFileList = async () => {
   const data = await requestData("/file/list", []);
   return JSON.parse(data);
 };
+const getPlanList = async (file: string) => {
+    const data = await requestData("/plan/list", [["file", file]]);
+    return JSON.parse(data);
+}
+const getCutList = async (file: string, plan: number) => {
+    const data = await requestData("/cut/list", [["file", file], ["plan", plan.toString()]]);
+    return JSON.parse(data);
+}
+const getCutData = async (file: string, plan: number, cut: number) => {
+    const data = await requestData("/cut/data", [["file", file], ["plan", plan.toString()], ["cut", cut.toString()]]);
+    return JSON.parse(data);
+}
+const getSpeedList = async (file: string, plan: number, cut: number, device: number) => {
+    const data = await requestData("/speed/list", [["file", file], ["plan", plan.toString()], ["cut", cut.toString()], ["device", device.toString()]]);
+    return JSON.parse(data);
+}
+const getDeviceList = async () => {
+    const data = await requestData("/device/list", []);
+    return JSON.parse(data);
+}
